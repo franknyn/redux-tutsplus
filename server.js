775e67cb9fea8a032c462eb5f767b8fc9b93b716
Frank <franknyn@yahoo.com>
2016-09-18 01:01:45 +1200

    to open browser automatically

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var open = require('open'); //for opening in browser
 var data = {};
-
+var port=3333;
 express()
   .use(express.static(__dirname + '/public'))
   .use(bodyParser.json())
   .get('/api/data', (req, res) => res.json(data))
   .post('/api/data', (req, res) => res.json(data = req.body))
   .get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'))
-  .listen(3333);
+  .listen(3333,  function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
